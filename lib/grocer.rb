089efea9b813630d25a59c0d8643ff9bb3c7bb82
Daniel Sasse <dsasse07@gmail.com>
2020-11-23 03:52:53 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -15,19 +15,31 @@ def consolidate_cart(cart)
   # REMEMBER: This returns a new Array that represents the cart. Don't merely
   # change `cart` (i.e. mutate) it. It's easier to return a new thing.
   
-  consolidated_cart = []
-  cart.each do |cart_item|
-    i=0 
-    if consolidated_cart[i].has_value? (cart_item[:item])
-      item_location = consolidated_cart.index(cart_item)
-      consolidated_cart[item_location][:count] += 1
-    else
-      cart_item[:count] = 1 
-      consolidated_cart << cart_item
-    end
-    i += 1
-  end
-    binding.pry
-    consolidated_cart
+  consolidated_cart = cart.uniq 
+  index = 0 
+  while index < consolidated_cart.length do 
+    consolidated_cart[0][:count] = 1 
+  end 
+  binding.pry
+      
+  
+  
+  
+  
+  
+  # consolidated_cart = []
+  # cart.each do |cart_item|
+  #   i=0 
+  #   if consolidated_cart[i].has_value? (cart_item[:item])
+  #     item_location = consolidated_cart.index(cart_item)
+  #     consolidated_cart[item_location][:count] += 1
+  #   else
+  #     cart_item[:count] = 1 
+  #     consolidated_cart << cart_item
+  #   end
+  #   i += 1
+  # end
+  #   binding.pry
+  #   consolidated_cart
 end
 
